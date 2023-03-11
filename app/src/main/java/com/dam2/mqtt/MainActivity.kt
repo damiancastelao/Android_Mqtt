234c759a3580a6da870b9c88a5329cf5671b8485
Damian Nogueiras <damiannogueiras@gmail.com>
2023-03-11 18:31:31 +0100

    Metodos básicos, conexión, subscripción y recepción

--- app/src/main/java/com/dam2/mqtt/MainActivity.kt
+++ app/src/main/java/com/dam2/mqtt/MainActivity.kt
@@ -1,11 +1,84 @@
 package com.dam2.mqtt
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import org.eclipse.paho.android.service.MqttAndroidClient
+import org.eclipse.paho.client.mqttv3.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mqttClient: MqttAndroidClient
+    private lateinit var mensaje: TextView
+
+    // variables
+    companion object {
+        const val TAG = "AndroidMqttClient"
+        val serverUri = "tcp://192.168.1.152:1883"
+        val topic = "hello"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mensaje = findViewById(R.id.mensaje)
+
+        connect(this)
+
+    }
+
+    fun connect(context: Context) {
+        mqttClient = MqttAndroidClient(context, serverUri, "kotlin_client")
+        mqttClient.setCallback(object : MqttCallback {
+            override fun messageArrived(topic: String?, message: MqttMessage?) {
+                Log.d(TAG, "Receive message: ${message.toString()} from topic: $topic")
+                mensaje.text = message.toString()
+            }
+
+            override fun connectionLost(cause: Throwable?) {
+                Log.d(TAG, "Connection lost ${cause.toString()}")
+            }
+
+            override fun deliveryComplete(token: IMqttDeliveryToken?) {
+
+            }
+        })
+        val options = MqttConnectOptions()
+        options.isAutomaticReconnect = true
+        options.isCleanSession = false
+        try {
+            mqttClient.connect(options, null, object : IMqttActionListener {
+                override fun onSuccess(asyncActionToken: IMqttToken?) {
+                    Log.d(TAG, "Connection success")
+                    mensaje.text = "Conectado"
+                    subscribe(topic)
+                }
+
+                override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
+                    Log.d(TAG, "Connection failure")
+                    Log.d(TAG, exception?.message.toString())
+                }
+            })
+        } catch (e: MqttException) {
+            e.printStackTrace()
+        }
+    }
+
+    fun subscribe(topic: String, qos: Int = 1) {
+        try {
+            mqttClient.subscribe(topic, qos, null, object : IMqttActionListener {
+                override fun onSuccess(asyncActionToken: IMqttToken?) {
+                    Log.d(TAG, "Subscribed to $topic")
+                }
+
+                override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
+                    Log.d(TAG, "Failed to subscribe $topic")
+                }
+            })
+        } catch (e: MqttException) {
+            e.printStackTrace()
+        }
     }
 }
